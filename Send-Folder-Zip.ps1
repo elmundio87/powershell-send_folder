572579fbb4b2a1b4dd00979db199280cc3db1096
Edmund Dipple <elmundio1987@gmail.com>
2015-03-23 12:30:25 +0000

    Clean up after itself

--- Send-Folder-Zip.ps1
+++ Send-Folder-Zip.ps1
@@ -6,7 +6,7 @@ $files = Get-ChildItem C:\Development\lightyear\chef-repo -File -Recurse
 $remote = "tfsbuild2"
 $session = new-pssession $remote
 
-remove-item "C:\tmp\send-file.zip" -force
+Remove-Item "C:\tmp\send-file.zip" -force
 
 Add-Type -Assembly "System.IO.Compression.FileSystem" ;
 [System.IO.Compression.ZipFile]::CreateFromDirectory("C:\Development\lightyear\chef-repo", "C:\tmp\send-file.zip");
@@ -14,10 +14,13 @@ Add-Type -Assembly "System.IO.Compression.FileSystem" ;
 sendfile -Source "C:\tmp\send-file.zip" -Destination "C:\tmp\send-file.zip" -Session $session
 
 Invoke-Command -Session $session -ScriptBlock {
+    Remove-Item -Recurse "C:\chef-repo"
     Add-Type -Assembly "System.IO.Compression.FileSystem" ;
     [System.IO.Compression.ZipFile]::ExtractToDirectory("C:\tmp\send-file.zip", "c:\chef-repo");
+    Remove-Item "C:\tmp\send-file.zip" -force
 }
 
+Remove-Item "C:\tmp\send-file.zip" -force
 Disconnect-PSSession $session
 
 $sw.Stop()
